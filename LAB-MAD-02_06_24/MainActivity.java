eff4db5ae645ce4598f085d7f4e6f662015a1726
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2024-07-02 13:48:03 +0530

    Create MainActivity.java

---
+++ LAB-MAD-02_06_24/MainActivity.java
@@ -0,0 +1,36 @@
+package com.chingkhei.edit_text0207;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString().trim();
+                if (!url.isEmpty()) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
