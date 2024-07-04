0f05aa8bb572bb73c29de77aa76dfaca550137a2
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2024-07-04 21:07:03 +0530

    Create MainActivity.java

---
+++ MAD-LAB-04_07_2024/MainActivity.java
@@ -0,0 +1,45 @@
+package com.chingkhei.edittext0407;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1;
+    EditText e2;
+    TextView tv1;
+    TextView tv2;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        e1.findViewById(R.id.edit1);
+        e2.findViewById(R.id.edit2);
+        tv1.findViewById(R.id.text1);
+        tv2.findViewById(R.id.text2);
+        submit.findViewById(R.id.button);
+
+        submit.setOnClickListener();
+        @Override
+        public void 
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
