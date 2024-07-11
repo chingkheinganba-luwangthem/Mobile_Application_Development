4640cc9e132f80fa1ddea6f612128e06c9bf428c
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2024-07-11 20:31:35 +0530

    Create MainActivity.java

---
+++ MAD-LAB-11-07-24/MainActivity.java
@@ -0,0 +1,38 @@
+package com.chingkhei.imageupload;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageview;
+    Button button;
+    private int[] images ={
+            R.mipmap.img6,
+            R.mipmap.img1,
+            R.mipmap.img2,
+            R.mipmap.img3,
+            R.mipmap.img4,
+            R.mipmap.img5,
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
